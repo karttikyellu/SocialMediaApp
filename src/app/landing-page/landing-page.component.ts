148eac069e1c7a79e994973cd369f0147f81054e
YELLU KARTTIK <yellukarttik@gmail.com>
2019-04-19 19:45:08 -0500

    Updated Landing Page

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -10,11 +10,34 @@ import { PrimaryKeyServiceService } from '../primary-key-service.service';
 })
 export class LandingPageComponent implements OnInit {
   primaryKey:string="";
+  emailId:string;
+  name:string;
+  ProfilePicture:string;
+  address:string;
+  dob:any;
   constructor( private http: HttpClient , private router: Router , private primaryKeyService: PrimaryKeyServiceService ) {
    }
 
   ngOnInit() {
     this.primaryKey = this.primaryKeyService.getPrimaryKey();
+    this.emailId = this.primaryKeyService.getEmailId();
+    let obs = this.http.get('http://localhost:3000/person/specificUserInfo/' + this.emailId);
+    obs.subscribe((data: any) => {
+        this.name = data.userModel.name;
+        this.address = data.userModel.address;
+        this.dob = data.dateOfBirth;
+        if (data.userModel.personPic != null) {
+          if (data.userModel.personPic.endsWith('.JPG') || data.userModel.personPic.endsWith('.jpg') || data.userModel.personPic.endsWith('.png')) {
+            console.log('Image ...' + 'http://localhost:3000/uploads/' + data.userModel.personPic);
+            this.ProfilePicture = 'http://localhost:3000/uploads/' + data.userModel.personPic;
+          } else {
+            this.ProfilePicture = 'http://localhost:3000/uploads/default.jpeg';
+          }
+        }
+
+      });
+
+
   }
 
 }
